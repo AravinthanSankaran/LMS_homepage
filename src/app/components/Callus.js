6346d879e1aac2102c3e38114bb0787c40ba1b7b
aravinthansankaran <aravinthanrsnkl@gmail.com>
2024-08-03 21:11:17 +0530

    callus changes

--- src/app/components/Callus.js
+++ src/app/components/Callus.js
@@ -1,7 +1,7 @@
 const CallButton = () => {
     return (
       <div className="fixed bottom-8 left-4">
-        <a href="tel:+917736474635" className="bg-primary text-white p-3 rounded-full shadow-lg">
+        <a href="tel:+911234567890" className="bg-primary text-white p-3 rounded-full shadow-lg">
           Call Us
         </a>
       </div>
--- src/app/components/Program/ProgramCard.js
+++ src/app/components/Program/ProgramCard.js
@@ -3,9 +3,9 @@ import React from "react";
 
 function BrandCard({ name, image }) {
   return (
-    <div className="border w-[350px] rounded-lg">
+    <div className="border w-[300px] rounded-lg">
       <div className="relative">
-        <Image src={image} alt={name} className="w-full h-auto object-cover" />
+        <Image src={image} alt={name} />
       </div>
       <div className="space-y-2 px-3">
         <h2 className="text-base text-heading font-semibold pt-2">{name}</h2>
